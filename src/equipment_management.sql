2766c32f47688ac45574fe468edb96bf96ea8403
tajwar679x <tajwar.khan@ada.ac.uk>
2025-02-18 19:43:07 +0000

    update equipment management

--- src/equipment_management.sql
+++ src/equipment_management.sql
@@ -14,3 +14,18 @@
 
 -- 3. Calculate average age of equipment by type (in days)
 -- TODO: Write a query to calculate average age of equipment by type (in days)
+
+-- 1. Find equipment due for maintenance
+SELECT equipment_id, name, next_maintenance_date
+FROM equipment
+WHERE next_maintenance_date BETWEEN DATE('now') AND DATE('now', '+30 days');
+
+-- 2. Count equipment types in stock
+SELECT type, COUNT(*) AS count
+FROM equipment
+GROUP BY type;
+
+-- 3. Calculate average age of equipment by type (in days)
+SELECT type, ROUND(AVG(JULIANDAY(CURRENT_DATE) - JULIANDAY(purchase_date)), 0) AS avg_age_days
+FROM equipment
+GROUP BY type;
